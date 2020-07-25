6efaa0025134045301e6838552add4017b6b6f16
mijailstell <mijailstell@gmail.com>
2020-07-25 14:46:34 -0500

    babel.config vacío

--- serverKoa.js
+++ serverKoa.js
@@ -1,10 +1,16 @@
-// import Koa from 'Koa';
-// import router from 'koa-router';
+// import Koa from 'koa';
+// import KoaRouter from 'koa-router';
 var Koa = require('Koa');
-var Router = require('koa-router')
+var Router = require('koa-router');
 
 var app = new Koa();
 var router = new Router();
+
+const middlewareKoa = (ctx, next) => {
+    console.log("LOGGER ", ctx.request.url);
+    next();
+};
+app.use(middlewareKoa);
 
 router.get('/usersKoa', (ctx, next) => {
     ctx.body = 'post hello koa';
